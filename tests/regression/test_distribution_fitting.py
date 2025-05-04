44822bf7846c6f80ad962b7cdf24b9e59bf46b77
Kyle <williams.kyle@gmx.com>
2025-05-04 14:43:22 -0500

    Fix tests

--- tests/regression/test_distribution_fitting.py
+++ tests/regression/test_distribution_fitting.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import pytest
 from smfs.estimation.estimate_lambda_mew import estimate_lambda, compute_poisson_distribution
-
+from smfs.estimation.estimate_lambda_mew_sd import estimate_lambda_sd, compute_gamma_distribution
 DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "test_data")
 
 @pytest.fixture
@@ -24,7 +24,7 @@ def test_poisson_lambda(poisson_data):
     mu_seg = input_df[input_df["AC"] != 0]["Mutation rate"].to_numpy()
     mu_non_seg = input_df[input_df["AC"] == 0]["Mutation rate"].to_numpy()
 
-    lam = estimate_lambda(mu_seg, mu_non_seg, method="poisson")
+    lam = estimate_lambda(mu_seg, mu_non_seg, initial_lambda=1e8)
     assert round(lam, 8) == round(expected_lambda, 8)
 
 def test_poisson_distribution(poisson_data):
@@ -42,17 +42,19 @@ def test_poisson_distribution(poisson_data):
 
 def test_gamma_lambda(gamma_data):
     input_df, _, expected_lambda = gamma_data
-    mu_seg = input_df[input_df["AC"] != 0]["Mutation rate"].to_numpy()
-    mu_non_seg = input_df[input_df["AC"] == 0]["Mutation rate"].to_numpy()
+    mu_seg = input_df[input_df["AC"] != 0]["Mean theta"].to_numpy()
+    mu_non_seg = input_df[input_df["AC"] == 0]["Mean theta"].to_numpy()
+    sig_seg = input_df[input_df["AC"] != 0]["SD theta"].to_numpy()
+    sig_non_seg = input_df[input_df["AC"] == 0]["SD theta"].to_numpy()
 
-    lam = estimate_lambda(mu_seg, mu_non_seg, method="gamma")
+    lam = estimate_lambda_sd(mu_seg, mu_non_seg, sig_seg, sig_non_seg, initial_lambda=1e3)
     assert round(lam, 8) == round(expected_lambda, 8)
 
 def test_gamma_distribution(gamma_data):
     input_df, expected_output, expected_lambda = gamma_data
     unq_mut_limit = len(expected_output) - 1
 
-    result = compute_poisson_distribution(input_df, expected_lambda, unq_mut_limit)
+    result = compute_gamma_distribution(input_df, expected_lambda, unq_mut_limit)
 
     pd.testing.assert_frame_equal(
         result.round(8),
